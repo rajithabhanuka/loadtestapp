161fdb10ac373913582df9ade03de0fe2fb982f2
rajithabhanuka1@gmail.com <halovitage.bhanuka@pearson.com>
2020-03-24 14:56:14 +0530

    firs commit

---
+++ src/main/java/com/java/loadtestapplication/LoadtestapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.java.loadtestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadtestapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadtestapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/loadtestapplication/LoadtestapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.loadtestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadtestapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
